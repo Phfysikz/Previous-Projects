8669b4a8659447e2915b9ffb9cc5d4604203be48
Phfysikz <99831251+Phfysikz@users.noreply.github.com>
2022-02-17 16:52:51 -0500

    Update and rename SquareCubePowerMultiplier to SquareCubePowerMultiplier.java

--- SquareCubePowerMultiplier.java
+++ SquareCubePowerMultiplier.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package squarecubepowermultiplier;
 
-/**
- *
- * @author HP USER
- */
+package squarecubepowermultiplier;
 
 import java.util.Scanner; // imports the scanner which will be used
 
